3f3ee4af1d4fe62b91e82a56a727c09c2d417658
kakekong <64763960+kakekong@users.noreply.github.com>
2025-12-01 13:57:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import server
+import threading, signal, sys
+from server import app       # webhook receiver
+from telegrambot import poll # command handler & alert creator
 
-if __name__ == '__main__':
-    server.start_server()
+if __name__ == "__main__":
+    # 1. webhook server in thread
+    t1 = threading.Thread(target=lambda: app.run(host="0.0.0.0", port=8000, debug=False), daemon=True)
+    t1.start()
+    # 2. telegram polling (blocking)
+    poll()
